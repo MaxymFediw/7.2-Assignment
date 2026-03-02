8094bd619b8088ddd08318dfafe11621cc2662ca
Maxym Fediw <362706764@GoTVDSB.ca>
2026-03-02 13:01:09 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _7._2_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
